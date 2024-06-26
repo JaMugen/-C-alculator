2132a10511d7dcef44a4cdcee496096d20085d76
Christopher Gaudious <crg33@njit.edu>
2024-06-26 10:03:54 -0400

    Created calculator C file.

---
+++ calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long int first_opperand;
+    printf("Enter the opparand, then the operator, and lastly the final opperand to get answer. \n");
+    scanf("%d", &first_opperand);
+
+    return 0;
+}
